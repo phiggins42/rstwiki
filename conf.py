47c5e7fd6c7bff05a1e9a601452f5847ddd29a05
Peter E Higgins <dante@dojotoolkit.org>
2011-02-06 10:12:50 -0500

    submodules ftw?

--- conf.py
+++ conf.py
@@ -1,16 +1,34 @@
 #
 #   rstwiki config stuffs
 #
-
 wiki = {
+
+    # where the .rst tree will live. /sample_data/ has some test .rst. 
+    # Suggest changing this to your own empty path to start editing 
+    # "homepage" (/index.rst) 
     "RST_ROOT":"./sample_data/",
-    #"RST_ROOT": "/Users/dante/www/dojo/docs/_source-moin/",           # where the .rst tree will live. /sample_data/ has some test .rst. Suggest changing this to your own
-    "SRC_VCS": "local",                     # enum(git, svn, local)
-    "SRC_REPO": "{some github url}",        # ignored if SRC_VCS is local
-    "USE_LDAP": False,                      # set to `True` if wanting to do simple auth against an LDAP server
-    "COMMIT_ONSAVE": True,                  # set to `False` if you want to manually push/commit changes, or if local
-    "LISTEN_PORT": 4200,                    # if serving directly, listen on this port
-    "STATIC_ROOT": "./_static/"             # where files served from /_static/ root
+    
+    # enum(git, svn, local)
+    "SRC_VCS": "local",
+    
+    # ignored if SRC_VCS is local
+    # if svn, commit happens directly on tree
+    # if git, commit happens direclly tree (?)
+    "SRC_REPO": "{some github|svn repo}",
+    
+    # set to `True` if wanting to do simple auth against an LDAP server
+    "USE_LDAP": False,
+    
+    # set to `False` if you want to manually push/commit changes, or if local
+    "COMMIT_ONSAVE": True,
+    
+    # if serving directly, listen on this port
+    "LISTEN_PORT": 4200,
+    
+    # where files served from /_static/ root live
+    # note: it's probably better to proxypass this or the like
+    "STATIC_ROOT": "./_static/"
+    
 }
 
 # add our src/ tree to the import path
