ba0d3a0e31baf0da53241a7e247de8bb06e3c5e9
Peter E Higgins <dante@dojotoolkit.org>
2011-02-13 16:46:56 -0500

    css cleanups

--- _static/docs/upload.js
+++ _static/docs/upload.js
@@ -2,13 +2,13 @@ dojo.provide("docs.upload");
 dojo.require("dojo.fx");
 dojo.ready(function(){
     
-    var template = "<div><label for='uploadfile{count}'>Select File:</label>" +
+    var template = "<div class='bloc'><label for='uploadfile{count}'>Select File:</label>" +
                 "<input type='file' name='uploadfile{count}' id='uploadfile{count}'></div>"
 
     var count = 1;
     dojo.query("#morefiles").onclick(function(e){
         e.preventDefault()
-        var n = dojo.place(dojo.replace(template, { count: count++ }), "files");
+        var n = dojo.place(dojo.replace(template, { count: count++ }), "files", "first");
         dojo.style(n, "height", "1px");
         dojo.fx.wipeIn({ node:n }).play()
     })
