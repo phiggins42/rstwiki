05a1292353d4d221a16ae9c39b8f00e22ff33eb5
Peter E Higgins <dante@dojotoolkit.org>
2011-02-13 20:25:13 -0500

    move this check inline

--- conf.sample.py
+++ conf.sample.py
@@ -36,6 +36,4 @@ wiki = {
 # add our src/ tree to the import path
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-
-if wiki['USE_LDAP']: import ldapauth as auth
 import directives
